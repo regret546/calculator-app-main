02548defacb75d6dac9719a886c9d063933ea4c5
regret546 <iregret546@gmail.com>
2024-04-27 19:09:08 +0800

    Refractor code

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,10 @@
-const getElement = (selector) => document.querySelector(selector);
-const getAllElements = (selector) => document.querySelectorAll(selector);
+const getElement = function (selector) {
+  return document.querySelector(selector);
+};
+
+const getAllElements = function (selector) {
+  return document.querySelectorAll(selector);
+};
 
 const previous = getElement("#previousNum");
 const current = getElement("#currentNum");
@@ -15,57 +20,88 @@ let previousNum = "";
 let currentNum = "";
 let currentOperator = "";
 
-const reset = () => {
-  [total, previousNum, currentNum, currentOperator] = ["", "", "", ""];
+const reset = function () {
+  total = "";
+  previousNum = "";
+  currentNum = "";
+  currentOperator = "";
   previous.innerText = "0";
 };
 
-const resolve = () => {
-  if (currentOperator === "x") currentOperator = "*";
-  const answer = eval(`${previousNum}${currentOperator}${currentNum}`);
-  [previousNum, currentNum, currentOperator] = [answer, "", ""];
+const resolve = function () {
+  if (currentOperator === "x") {
+    currentOperator = "*";
+  }
+  const answer = eval(previousNum + currentOperator + currentNum);
+  if (answer === 0) {
+    previousNum = "";
+  } else if (answer.length > 16) {
+    alert("Maximum number reach, calculator will reset");
+    reset();
+    checkAlwaysTheValue();
+    return;
+  }
+  previousNum = answer;
+  currentNum = "";
+  currentOperator = "";
   previous.innerText = previousNum;
   checkAlwaysTheValue();
 };
 
-const checkAlwaysTheValue = () => {
-  previous.textContent = previousNum === "" ? "0" : previousNum;
-  current.innerText = currentNum === "" ? "" : currentNum;
-  operator.innerText = currentOperator === "" ? "" : currentOperator;
+const checkAlwaysTheValue = function () {
+  if (previousNum === "") {
+    previous.textContent = "0";
+  } else {
+    previous.textContent = previousNum;
+  }
+  if (currentNum === "") {
+    current.innerText = "";
+  } else {
+    current.innerText = currentNum;
+  }
+  if (currentOperator === "") {
+    operator.innerText = "";
+  } else {
+    operator.innerText = currentOperator;
+  }
 };
 
-allNums.forEach((num) =>
-  num.addEventListener("click", () => {
-    try {
-      if (!previousNum || !currentOperator) {
-        previousNum +=
-          num.innerText === "." ? (previousNum ? "" : "0.") : num.innerText;
-        previous.innerText = previousNum;
-      } else {
-        currentNum +=
-          num.innerText === "." ? (currentNum ? "" : "0.") : num.innerText;
-        current.innerText = currentNum;
-      }
-    } catch (e) {
-      alert("Error, Please click reset button");
-      console.log(e);
+allNums.forEach((num) => {
+  num.addEventListener("click", function () {
+    if (previousNum.length > 7 || currentNum.length > 7) {
+      alert("Maximum number reach, calculator will reset");
+      reset();
+      return;
     }
-  })
-);
-
-allOperators.forEach((operators) =>
-  operators.addEventListener("click", () => {
+    if (previousNum === "" || currentOperator === "") {
+      previousNum += num.innerText;
+      previous.innerText = previousNum;
+    }
+    if (previousNum !== "" && currentOperator) {
+      currentNum += num.innerText;
+      current.innerText = currentNum;
+    }
     checkAlwaysTheValue();
+  });
+});
+
+allOperators.forEach(function (operators) {
+  operators.addEventListener("click", function () {
+    checkAlwaysTheValue();
+    if (currentOperator) {
+      alert("You can't assign morethan 1 operator!");
+      return;
+    }
     currentOperator = operators.innerText;
     operator.innerText = currentOperator;
-  })
-);
+  });
+});
 
 equalBtn.addEventListener("click", resolve);
 
 resetBtn.addEventListener("click", reset);
 
-deleteBtn.addEventListener("click", () => {
+deleteBtn.addEventListener("click", function () {
   if (previousNum && !currentOperator) {
     previousNum = parseInt(previousNum.toString().slice(0, -1));
     previous.innerText = previousNum || "0";
@@ -74,7 +110,13 @@ deleteBtn.addEventListener("click", () => {
     currentNum = parseInt(currentNum.toString().slice(0, -1));
     current.innerText = currentNum || "0";
   }
-  if (isNaN(previousNum)) [previousNum, previous.innerText] = ["", "0"];
-  if (isNaN(currentNum)) [currentNum, current.innerText] = ["", "0"];
+  if (isNaN(previousNum)) {
+    previousNum = "";
+    previous.innerText = "0";
+  }
+  if (isNaN(currentNum)) {
+    currentNum = "";
+    current.innerText = "0";
+  }
   checkAlwaysTheValue();
 });
